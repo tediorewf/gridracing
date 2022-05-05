41799ff1f996ae91c80ed7fc9862186d697887a3
Mikhail Eremeev <meremeev@sfedu.ru>
2022-05-05 16:33:13 +0300

    build(client): add copy-webpack-plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,8 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
-
 
 const config = {
     entry: './src/index.js',
@@ -19,28 +17,24 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        new CopyPlugin({
+            patterns: [
+                {
+                  from: path.resolve(__dirname, 'assets/**/*'),
+                  to: path.resolve(__dirname, 'dist')
+                },
+                {
+                    from: path.resolve(__dirname, 'public/**/*'),
+                    to: path.resolve(__dirname, 'dist')
+                },
+            ],
+        }),
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
 };
 
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
     } else {
         config.mode = 'development';
     }
