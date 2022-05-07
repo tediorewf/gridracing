e6043c27d1c158db3e99a6d8634451972815563f
Mikhail Eremeev <meremeev@sfedu.ru>
2022-05-07 20:34:27 +0300

    feat: add server

---
+++ server/index.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const xDefault = 1,
+      yDefault = 1,
+      angleDefault = 0;
+
+class Car {
+    constructor(x, y, angle) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+    }
+
+    move(x, y, angle) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+    }
+
+    static spawn() {
+        let spawned = new Car(xDefault, yDefault, angleDefault);
+        return spawned;
+    }
+}
+
+let cars = new Map();
+
+const httpServer = createServer();
+const serverConfig = {
+    cors: {
+        origin: '*'
+    }
+};
+const io = new Server(httpServer, serverConfig);
+
+io.on('connection', (socket) => {
+    console.log(`Client has connected: ${socket.id}`);
+
+    socket.emit('setId');
+
+    cars.forEach((car, id) => {
+        console.log(`Transmit car for client ${id}: x=${car.x}, y=${car.y}, angle=${car.angle}`)
+        socket.emit('spawnCar', id, car.x, car.y, car.angle);
+    });
+
+    let car = Car.spawn();
+    cars.set(socket.id, car);
+    console.log(`Spawned car for client ${socket.id}: x=${car.x}, y=${car.y}, angle=${car.angle}`);
+    io.emit('spawnCar', socket.id, car.x, car.y, car.angle);
+
+    socket.on('moveCar', (x, y, angle) => {
+        let car = cars.get(socket.id);
+        car.move(x, y, angle);
+        console.log(`Client ${socket.id} has moved his car: x=${x}, y=${y}, angle=${angle}`);
+        io.emit('moveCar', socket.id, x, y, angle);
+    });
+
+    socket.on('disconnect', (reason) => {
+        console.log(`Client ${socket.id} has disconnected: ${reason}`);
+        cars.delete(socket.id);
+        io.emit('killCar', socket.id);
+    });
+});
+
+const port = 3000;
+httpServer.listen(port, () => {
+    console.log('Server has started!');
+});
